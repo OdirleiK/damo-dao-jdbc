6ec98b3bd400d188f555854b3b051abfff159827
Odirlei Kempfer <odirlei67777@gmail.com>
2022-09-12 07:10:04 -0300

    Department and Seller Dao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import model_entities.Department;
+import model_entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -8,5 +11,12 @@ public class Program {
 
         Department obj = new Department(1, "books");
         System.out.println(obj);
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
+
+
+
    }
+
+
 }
