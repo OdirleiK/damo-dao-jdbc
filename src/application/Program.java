7ace956253b2600cb955fb42f9b4376c15bb52d2
Odirlei Kempfer <odirlei67777@gmail.com>
2022-09-12 07:44:51 -0300

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model_dao.DaoFactory;
+import model_dao.SellerDao;
 import model_entities.Department;
 import model_entities.Seller;
 
@@ -9,9 +11,10 @@ public class Program {
 
     public static void main(String[]args){
 
-        Department obj = new Department(1, "books");
-        System.out.println(obj);
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
 
 
