16df41a3749ce6a408b4952939d680b161937f24
Odirlei Kempfer <odirlei67777@gmail.com>
2022-09-12 14:55:52 -0300

    findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model_entities.Department;
 import model_entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -13,11 +14,16 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== Teste 1: findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
 
-
+        System.out.println("=== Teste 2: seller ByFindDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list) {
+            System.out.println(obj);
+        }
 
    }
 
